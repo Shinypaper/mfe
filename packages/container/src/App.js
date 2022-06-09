225bcd0286e7e3998792a071bbe5fae701399eb6
Manfred Headley <mheadley@collectivei.com>
2022-06-09 13:21:02 -0400

    css fix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core'
 import Header from './components/Header'
 import MarketingApp from './components/MarketingApp'
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+})
+
 export default () => {
-  return <BrowserRouter>
+  return <StylesProvider generateClassName={generateClassName}>
+  <BrowserRouter>
     <Header />
     <MarketingApp />
   </BrowserRouter>
+  </StylesProvider>
 }

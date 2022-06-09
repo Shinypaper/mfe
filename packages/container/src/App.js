07d486130685a950dcca445d470f37103b7e9f56
Manfred Headley <mheadley@collectivei.com>
2022-06-09 12:59:46 -0400

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Header from './components/Header'
 import MarketingApp from './components/MarketingApp'
 
 export default () => {
-  return <>
-    <h1>Navigation might go here</h1>
-    <hr></hr>
+  return <BrowserRouter>
+    <Header />
     <MarketingApp />
-  </>
-  
+  </BrowserRouter>
 }
